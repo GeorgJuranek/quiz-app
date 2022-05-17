3cd755f9514f04fb64f72fd886f55037094efbae
Georg Juranek <georg_juranek@hotmail.de>
2022-05-17 09:36:39 +0200

    Javascript first steps

---
+++ test.js
@@ -0,0 +1,15 @@
+const submitButton = document.querySelector('[data-js="submitButton"]');
+
+submitButton.addEventListener("click", onSubmitClick);
+
+function onSubmitClick() {
+  //console.log("Button wird erkannt");
+  submitButton.classList.toggle("testJs-class");
+
+  if (submitButton.innerHTML == "SUBMIT") {
+    console.log("wird veraendert");
+    submitButton.innerHTML = "SUBMITED";
+  } else {
+    submitButton.innerHTML = "SUBMIT";
+  }
+}
