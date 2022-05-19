8415b295038486a1565ef1640e601d8457c0b125
Georg Juranek <georg_juranek@hotmail.de>
2022-05-19 14:12:51 +0200

    Linting

--- javascript/mainManager.js
+++ javascript/mainManager.js
@@ -8,50 +8,32 @@ const mainBookmarks = document.querySelector('[data-js="mainBookmarks"]');
 const mainCreate = document.querySelector('[data-js="mainCreate"]');
 const mainProfile = document.querySelector('[data-js="mainProfile"]');
 
-console.log("Hello");
+console.log('Hello');
 
-/*function changeMain(main) {
-  
-    console.log("main:" + main.type);
+homeButton.addEventListener('click', () => {
+  mainIndex.classList.remove('hide-it');
+  mainBookmarks.classList.add('hide-it');
+  mainCreate.classList.add('hide-it');
+  mainProfile.classList.add('hide-it');
+});
 
-  mainIndex.classList.remove("active");
-  mainIndex.classList.add("hide-it");
+bookmarksButton.addEventListener('click', () => {
+  mainIndex.classList.add('hide-it');
+  mainBookmarks.classList.remove('hide-it');
+  mainCreate.classList.add('hide-it');
+  mainProfile.classList.add('hide-it');
+});
 
-  mainBookmarks.classList.remove("active");
-  mainBookmarks.classList.add("hide-it");
+createButton.addEventListener('click', () => {
+  mainIndex.classList.add('hide-it');
+  mainBookmarks.classList.add('hide-it');
+  mainCreate.classList.remove('hide-it');
+  mainProfile.classList.add('hide-it');
+});
 
-  mainCreate.classList.remove("active");
-  mainCreate.classList.add("hide-it");
-
-  mainProfile.classList.remove("active");
-  mainProfile.classList.add("hide-it");
-
-
-  main.classList.remove("hide-it");
-  main.classList.add("active");
-}
-
-
-home.addEventListener("click", changeMain(mainIndex));
-bookmarks.addEventListener("click", changeMain(mainBookmarks));
-create.addEventListener("click", changeMain(mainCreate));
-profile.addEventListener("click", changeMain(mainProfile));
-*/
-
-function toggleDisplay() {
-  mainBookmarks.classList.toggle("hide-it");
-}
-bookmarksButton.addEventListener("click", toggleDisplay);
-
-function toggleDisplay() {
-  mainCreate.classList.toggle("hide-it");
-}
-createButton.addEventListener("click", toggleDisplay);
-
-//homeButton.addEventListener("click",);
-
-//createButton.addEventListener("click", toggleDisplay(mainCreate));
-
-//createButton.addEventListener("click",);
-
-//profileButton.addEventListener("click",);
+profileButton.addEventListener('click', () => {
+  mainIndex.classList.add('hide-it');
+  mainBookmarks.classList.add('hide-it');
+  mainCreate.classList.add('hide-it');
+  mainProfile.classList.remove('hide-it');
+});
--- javascript/test.js
+++ javascript/test.js
@@ -1,10 +1,10 @@
 const submitButton = document.querySelector('[data-js="submitButton"]');
 
-submitButton.addEventListener("click", onSubmitClick);
+submitButton.addEventListener('click', onSubmitClick);
 
 function onSubmitClick() {
   //console.log("Button wird erkannt");
-  submitButton.classList.toggle("testJs-class");
+  submitButton.classList.toggle('testJs-class');
 
   /*if (submitButton.innerHTML == "SUBMIT") {
     console.log("wird veraendert");
